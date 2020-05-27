dd24b95f07b72532db28b66ee32b98acdf4d6f2e
niteshpawar97 <niteshpawar97@gmail.com>
2020-05-27 22:01:42 +0530

    Initial commit

---
+++ app/src/main/java/com/store/cwa/Activity/ContectusActivity.java
@@ -0,0 +1,14 @@
+package com.store.cwa.Activity;
+
+import android.os.Bundle;
+
+import com.store.cwa.R;
+
+public class ContectusActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contectus);
+    }
+}
---
+++ app/src/main/java/com/store/cwa/Utils/LoopingCirclePageIndicator.java
@@ -0,0 +1,4 @@
+package com.store.cwa.Utils;
+
+public class LoopingCirclePageIndicator {
+}
